9576cf78d72a1d1208259c8bd85cc008fd847341
ezg27 <georgehams27@gmail.com>
2018-09-18 16:16:24 +0100

    setup yelp api call function

--- handler.js
+++ handler.js
@@ -1,10 +1,24 @@
 'use strict';
+const yelpKey = require('./config');
+const axios = require('axios');
 
-module.exports.getYelp = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello world!!!'
-    }),
+module.exports.getYelp = (event, context, callback) => {
+  const config = {
+    headers: {
+      Authorization: yelpKey
+    },
+    params: {
+      latitude: 53.4863,
+      longitude: -2.2397,
+      radius: 100,
+      categories: 'bars,restaurants'
+    }
   };
+  axios.get('https://api.yelp.com/v3/businesses/search', config).then(res => {
+    const obj = {
+      statusCode: 200,
+      body: JSON.stringify(res.data.businesses)
+    };
+    callback(null, obj);
+  })
 };
