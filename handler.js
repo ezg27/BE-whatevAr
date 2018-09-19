5f682e17b9dc5c8bf8c8a2311b171faf02075496
ezg27 <georgehams27@gmail.com>
2018-09-19 09:49:14 +0100

    refactor handler.js removing async await

--- handler.js
+++ handler.js
@@ -1,8 +1,3 @@
 'use strict';
 const { getYelp } = require('./api');
-
-module.exports.deviceData = async event => {
-  console.log(event)
-  const result = await getYelp(event.pathParameters);
-  return result;
-};
+module.exports.deviceData = event => getYelp(event.pathParameters);
