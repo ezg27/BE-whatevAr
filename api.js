5a3cd43d2102fa2649e31452ff27b2f8c975e62c
JanMach97 <janmachacek97@gmail.com>
2018-09-20 14:34:45 +0100

    add testHour logic

--- api.js
+++ api.js
@@ -36,23 +36,30 @@ module.exports.getBusinessData = ({ id, name }) => {
       'x-api-version': 2
     }
   }
+  console.log(name);
   const getYelp = axios.get(`https://api.yelp.com/v3/businesses/${id}`, yConfig);
   const getFoodRating = axios.get(`http://api.ratings.food.gov.uk/Establishments?name=${name}&localAuthorityId=180`, rConfig)
   return Promise.all([getYelp, getFoodRating])
     .then(([{data}, RatingData]) => {
-      const hours = data.hours[0].open.map(day => {
-        return `${day.start.slice(0, 2)}:${day.start.slice(2)} - ${day.end.slice(0, 2)}:${day.end.slice(2)}`
-      })
+      // const hours = data.hours[0].open.map(day => {
+      //   return `${day.start.slice(0, 2)}:${day.start.slice(2)} - ${day.end.slice(0, 2)}:${day.end.slice(2)}`
+      // })
+
+      const testHOurs = !data.hours ? null : data.hours[0].open.reduce((acc, day) => {
+        acc[day.day] = `${day.start.slice(0, 2)}:${day.start.slice(2)} - ${day.end.slice(0, 2)}:${day.end.slice(2)}`
+
+        return acc;
+      }, {})
       const theData = {
         id: data.id,
         name: data.name,
         phone: data.phone,
         photos: data.photos,
-        isOpen: data.hours[0].is_open_now,
+        // isOpen: data.hours[0].is_open_now,
         url: data.url,
         reviewCount: data.review_count,
-        foodRating: RatingData.data.establishments[0].RatingKey,
-        hours
+        foodRating: RatingData.data.establishments[0].RatingKey ? RatingData.data.establishments[0].RatingKey : null,
+        hours: testHOurs
       }
       return {
         statusCode: 200,
