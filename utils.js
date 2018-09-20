b62227e3bba8be26710faae846d6a43d890017f5
ezg27 <georgehams27@gmail.com>
2018-09-20 16:39:15 +0100

    reorganise function order

--- utils.js
+++ utils.js
@@ -1,4 +1,26 @@
-const conversion = (businesses, latitude, longitude) => {
+const latLongToMerc = (lat, long) => {
+  const lon_rad = (long / 180.0) * Math.PI;
+  const lat_rad = (lat / 180.0) * Math.PI;
+  const sm_a = 6378137.0;
+  const xmeters = sm_a * lon_rad;
+  const ymeters = sm_a * Math.log((Math.sin(lat_rad) + 1) / Math.cos(lat_rad));
+  return { x: xmeters, y: ymeters };
+};
+
+const transformPointToAR = (
+  deviceLat,
+  deviceLong,
+  businessLat,
+  businessLong
+) => {
+  const objPoint = latLongToMerc(businessLat, businessLong);
+  const devicePoint = latLongToMerc(deviceLat, deviceLong);
+  const objFinalPosZ = objPoint.y - devicePoint.y;
+  const objFinalPosX = objPoint.x - devicePoint.x;
+  return [objFinalPosX, 0, -objFinalPosZ];
+};
+
+module.exports.conversion = (businesses, latitude, longitude) => {
   if (!businesses) return;
   return businesses.reduce((acc, business) => {
     business.position = transformPointToAR(
@@ -23,29 +45,7 @@ const conversion = (businesses, latitude, longitude) => {
   }, {});
 };
 
-const latLongToMerc = (lat, long) => {
-  const lon_rad = (long / 180.0) * Math.PI;
-  const lat_rad = (lat / 180.0) * Math.PI;
-  const sm_a = 6378137.0;
-  const xmeters = sm_a * lon_rad;
-  const ymeters = sm_a * Math.log((Math.sin(lat_rad) + 1) / Math.cos(lat_rad));
-  return { x: xmeters, y: ymeters };
-};
-
-const transformPointToAR = (
-  deviceLat,
-  deviceLong,
-  businessLat,
-  businessLong
-) => {
-  const objPoint = latLongToMerc(businessLat, businessLong);
-  const devicePoint = latLongToMerc(deviceLat, deviceLong);
-  const objFinalPosZ = objPoint.y - devicePoint.y;
-  const objFinalPosX = objPoint.x - devicePoint.x;
-  return [objFinalPosX, 0, -objFinalPosZ];
-};
-
-const formatHours = hoursData => {
+module.exports.formatHours = hoursData => {
   const days = hoursData.map(
     (day, i) =>
       i !== hoursData[i].day
@@ -55,5 +55,3 @@ const formatHours = hoursData => {
   while (days.length !== 7) days.push('Closed');
   return days;
 };
-
-module.exports = { conversion, formatHours };
