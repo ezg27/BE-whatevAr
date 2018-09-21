a067f3081d97ebee3bedd643bceeb5366be46d24
ezg27 <georgehams27@gmail.com>
2018-09-21 14:15:05 +0100

    add error handling to getYelp

--- api.js
+++ api.js
@@ -22,7 +22,15 @@ module.exports.getYelp = ({ lat, long }) => {
         body: JSON.stringify(conversion(res.data.businesses, lat, long))
       };
     })
-    .catch(() => console.log('egggggsssss'));
+    .catch(err => {
+      return {
+        statusCode: err.response.status,
+        body: JSON.stringify({
+          source: 'Yelp API',
+          message: err.response.statusText
+        })
+      };
+    });
 };
 
 module.exports.getBusinessData = ({ id, name }) => {
@@ -35,28 +43,37 @@ module.exports.getBusinessData = ({ id, name }) => {
     headers: {
       'x-api-version': 2
     }
-  }
-  const getYelp = axios.get(`https://api.yelp.com/v3/businesses/${id}`, yConfig);
-  const getFoodRating = axios.get(`http://api.ratings.food.gov.uk/Establishments?name=${name}&localAuthorityId=180`, rConfig)
+  };
+  const getYelp = axios.get(
+    `https://api.yelp.com/v3/businesses/${id}`,
+    yConfig
+  );
+  const getFoodRating = axios.get(
+    `http://api.ratings.food.gov.uk/Establishments?name=${name}&localAuthorityId=180`,
+    rConfig
+  );
   return Promise.all([getYelp, getFoodRating])
-    .then(([{data}, RatingData]) => {
+    .then(([{ data }, RatingData]) => {
       const theData = {
         id: data.id,
         name: data.name,
         phone: data.phone,
         photos: data.photos,
-        isOpen: data.hours ? data.hours[0].is_open_now : null, 
+        isOpen: data.hours ? data.hours[0].is_open_now : null,
         url: data.url,
         reviewCount: data.review_count,
-        foodRating: RatingData.data.establishments.length !== 0 ? RatingData.data.establishments[0].RatingKey : null,
+        foodRating:
+          RatingData.data.establishments.length !== 0
+            ? RatingData.data.establishments[0].RatingKey
+            : null,
         hours: !data.hours ? null : formatHours(data.hours[0].open),
         address: data.location.address1,
         postcode: data.location.zip_code
-      }
+      };
       return {
         statusCode: 200,
         body: JSON.stringify(theData)
-      }
+      };
     })
-    .catch(console.log)
-}
+    .catch(console.log);
+};
