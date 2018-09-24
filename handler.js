5c56a44512ccf012d9c81ca2debbc62c37ad5077
ezg27 <georgehams27@gmail.com>
2018-09-24 12:13:23 +0100

    add addUser function

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 'use strict';
 const { getYelp, getBusinessData } = require('./api');
+const { addUserData } = require('./dbManager');
 module.exports.deviceData = event => getYelp(event.pathParameters);
 module.exports.getBusiness = event => getBusinessData(event.pathParameters);
+module.exports.addUser = (event, context, callback) => addUserData(event, context, callback);
