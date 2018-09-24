46d279c175676c55b5cae94ccd4a009ca9696e0e
ezg27 <georgehams27@gmail.com>
2018-09-24 12:15:32 +0100

    create addUserData function

---
+++ dbManager.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.addUserData = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      username: data.username,
+      password: data.password,
+      createdAt: timestamp
+    }
+  };
+
+  dynamoDb.put(params, error => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't create the todo item."
+      });
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
+};
